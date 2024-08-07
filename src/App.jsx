4bb71f53bc2ac36866f8347490b8e661bab99d27
eid <eidhachem1@gmail.com>
2024-08-07 16:20:37 +0300

    Add Karla font

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import AddUsers from './components/Form/AddUsers'
 
-import { v4 as uuidv4 } from 'uuid'
 import UsersList from './components/List/UsersList'
-
-const DUMMY_USERS = [
-  {
-    id: uuidv4(),
-    name: 'Eid',
-    age: 32
-  },
-  {
-    id: uuidv4(),
-    name: 'Elie',
-    age: 30
-  },
-  {
-    id: uuidv4(),
-    name: 'John',
-    age: 31
-  },
-]
 
 function App() {
 
-  const [users, setUsers] = useState(DUMMY_USERS)
+  const [users, setUsers] = useState([])
 
   const updateUsersList = (user) => {
     setUsers((prevUsers) => {
@@ -36,7 +17,7 @@ function App() {
   return (
     <div>
     <AddUsers addUser={updateUsersList} />
-    <UsersList users={users} />
+    { users.length && <UsersList users={users} />}
     </div>
   )
 }
